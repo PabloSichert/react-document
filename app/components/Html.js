462cb6edc797f10e9fe57eb2cb01638293815515
Pablo Sichert <mail@pablosichert.de>
2016-10-06 21:59:09 +0200

    Unmount scripts after initial render

--- app/components/Html.js
+++ app/components/Html.js
@@ -1,39 +1,61 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { oneLine } from 'common-tags';
 
-export default function Html(props, context) {
-    const {
-        children,
-        params: {
-            title = 'React Document'
-        }
-    } = props;
+export default class Html extends Component {
+    constructor(...args) {
+        super(...args);
 
-    const {
-        scriptUrl,
-        protocol,
-        host
-    } = context;
+        this.state = {};
+    }
 
-    return (
-        <html>
-            <head>
-                <meta charSet='utf-8' />
-                <meta httpEquiv='x-ua-compatible' content='ie=edge' />
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <title>{title}</title>
-                <script dangerouslySetInnerHTML={{ __html: oneLine`
-                    window.scriptUrl = '${scriptUrl}';
-                    window.protocol = '${protocol}';
-                    window.host = '${host}';
-                `}} />
-                <script defer src={scriptUrl} />
-            </head>
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+    componentDidMount() {
+        this.setState({
+            mounted: true
+        });
+    }
+
+    render() {
+        const {
+            children,
+            params: {
+                title = 'React Document'
+            }
+        } = this.props;
+
+        const {
+            scriptUrl,
+            protocol,
+            host
+        } = this.context;
+
+        const {
+            mounted
+        } = this.state;
+
+        return (
+            <html>
+                <head>
+                    <meta charSet='utf-8' />
+                    <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+                    <meta name='viewport' content='width=device-width, initial-scale=1' />
+                    <title>{title}</title>
+                    {!mounted &&
+                        <script dangerouslySetInnerHTML={{ __html: oneLine`
+                            window.scriptUrl = '${scriptUrl}';
+                            window.protocol = '${protocol}';
+                            window.host = '${host}';
+                        `}} />
+                    }
+                    {!mounted &&
+                        <script defer src={scriptUrl} />
+                    }
+                </head>
+                <body>
+                    {children}
+                </body>
+            </html>
+        );
+    }
 }
 
 Html.propTypes = {
